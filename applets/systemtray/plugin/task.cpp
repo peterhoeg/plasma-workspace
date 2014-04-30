ba097fe2e7ace95b4b03c4ec95cefa356211a7f7
David Edmundson <kde@davidedmundson.co.uk>
2014-04-30 18:17:22 +0200

    Init variable Task::shown

--- applets/systemtray/plugin/task.cpp
+++ applets/systemtray/plugin/task.cpp
@@ -36,7 +36,8 @@ public:
     Private()
         : status(Task::UnknownStatus),
           category(Task::UnknownCategory),
-          expanded(false)
+          expanded(false),
+          show(false)
     {
     }
 
